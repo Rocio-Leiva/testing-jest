37de5bf2d669745ecb182a9fff5ae50112965ee9
Rocio-Leiva <rmleipec@gmail.com>
2023-01-08 11:52:51 +0100

    new lines

--- __tests__/validarFormulario.test.js
+++ __tests__/validarFormulario.test.js
@@ -2,5 +2,5 @@ import { validarFormulario } from '../src/validarFormulario';
 
 test("should return alert('Por favor, rellena todos los campos') if we pass ' ' " , () => {
     let result = validarFormulario('');
-    expect(result).toBe('Por favor, rellena todos los campos')
+    expect(result).toBe( alert('Por favor, rellena todos los campos'))
 })
